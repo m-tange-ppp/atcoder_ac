231db036badd15036af42ee22e2a9b199b0bbcc1
MiTange <tmrevo210@yahoo.co.jp>
2023-12-04 23:34:55 +0900

    new

---
+++ atcoder.jp/tessoku-book/tessoku_book_bg/Main.py
@@ -0,0 +1,50 @@
+import sys
+def II(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+def MI(): return map(int,sys.stdin.readline().rstrip().split())
+def SI(): return sys.stdin.readline().rstrip()
+def LS(): return list(sys.stdin.readline().rstrip().split())
+
+# セグメント木
+class segtree:
+	# 要素 dat の初期化を行う（最初は全部ゼロ）
+	def __init__(self, n):
+		self.size = 1
+		while self.size < n: #葉の数2^x
+			self.size *= 2
+		self.dat = [ 0 ] * (self.size * 2) #葉の数*2で全体のノード数
+	
+	# クエリ 1 (A[pos] = x)に対する処理
+	def update(self, pos, x):
+		pos += self.size # pos は 0-indexed なので、A[pos] のみに対応するセルの番号は pos + size
+		self.dat[pos] = x
+		while pos >= 2: #親に伝播
+			pos //= 2
+			self.dat[pos] = self.dat[pos * 2] + self.dat[pos * 2 + 1]
+	
+	# クエリ 2 ([l, r))に対する処理
+	# u は現在のセル番号、[a, b) はセルに対応する半開区間、[l, r) は求めたい半開区間
+	def query(self, l, r, a, b, u): # 再帰で完全2分木を探索
+		if r <= a or b <= l:
+			return 0 # 一切含まれない場合
+		if l <= a and b <= r:
+			return self.dat[u] # 完全に含まれる場合
+		m = (a + b) // 2
+		answerl = self.query(l, r, a, m, u * 2)
+		answerr = self.query(l, r, m, b, u * 2 + 1)
+		return answerl + answerr
+
+# 入力
+N, Q = MI()
+
+# クエリの処理
+Z = segtree(N)
+for _ in range(Q):
+	tp, *cont = MI()
+	if tp == 1:
+		pos, x = cont
+		Z.update(pos - 1, x) # pos は 1-indexed で入力されるので、update 関数の引数は pos - 1 にします
+	if tp == 2:
+		l, r = cont
+		answer = Z.query(l - 1, r - 1, 0, Z.size, 1) # 0-indexed の実装では、最初のセルに対応する半開区間は [0, size) です
+		print(answer)
